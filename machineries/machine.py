f63a6a8dfad2ed943f82c08ef42124210a186932
michael tadnir <tadnir50@gmail.com>
2023-05-22 21:43:11 +0300

    Fixing pylint errors and checking CI actions

--- machineries/machine.py
+++ machineries/machine.py
@@ -1,5 +1,30 @@
-
+"""This class defines the base machine object"""
 
 
 class Machine:
-    pass
+    """The base machine object"""
+
+    def __init__(self):
+        """
+        Create a new Machine object
+        
+        :return: The new machine object.
+        """
+
+        self.test = "Hello World"
+
+    def work1(self):
+        """
+        Do Some work.
+        
+        :return: Nothing
+        """
+        print(self.test)
+
+    def work2(self):
+        """
+        Do Some work.
+        
+        :return: The test string
+        """
+        return self.test
